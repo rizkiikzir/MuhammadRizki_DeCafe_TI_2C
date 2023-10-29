6e13f2a6c4ee846012409054d7a696c6166c1485
rizkiikzir <teek041030@gmail.com>
2023-10-29 12:07:20 +0700

    first commit

---
+++ order.php
@@ -0,0 +1,16 @@
+<div class="col-lg-9 mt-2">
+    <div class="card">
+        <div class="card-header">
+            Order
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Ini adalah bagian Order</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet voluptates ad fugit
+                obcaecati debitis harum quo a libero quis quae optio quam dolore accusantium quasi modi
+                nisi pariatur, maxime expedita.
+            </p>
+            <a href="#" class="btn btn-dark">Go somewhere</a>
+        </div>
+    </div>
+</div>
